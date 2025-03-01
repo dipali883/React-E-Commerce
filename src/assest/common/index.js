fd41fc63c4a1ee6d4d364c4d7a331c60977deb72
dipali883 <dipaligediya02101@gmail.com>
2025-03-01 18:50:39 +0530

    changes in signup form

---
+++ src/assest/common/index.js
@@ -0,0 +1,11 @@
+const backendDomain = "http://localhost:8080"
+
+// for api url
+const SummeryApi = {
+    signUp :{
+        url : `${backendDomain}/api/signup`,
+        method : "post" 
+    }
+}
+
+export default SummeryApi
